5b21432260a86ce61c717ab4c48e351292c6daae
yulyarudenko88 <yulyarudenko88@users.noreply.github.com>
2023-09-13 17:13:15 +0000

    deploy: 975836d1b5baa6bfa2bcf83ea4507d2f38f153df

---
+++ assets/Contacts-7a3a70a7.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d5f198bf.js";const o=()=>t.jsx("div",{children:"Contacts"});export{o as default};
---
+++ assets/Portfolio-e1905e45.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-d5f198bf.js";const t=()=>o.jsx("div",{children:"Portfolio"});export{t as default};
