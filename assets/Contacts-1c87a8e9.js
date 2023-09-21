09b36370b1900ea87461557284768b7d7264e001
yulyarudenko88 <yulyarudenko88@users.noreply.github.com>
2023-09-21 12:59:23 +0000

    deploy: a4604b75c675dbe66bcfc06bfecf6d1cb16c0e61

---
+++ assets/Contacts-1c87a8e9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-e92dd421.js";const o=()=>t.jsx("div",{children:"Contacts"});export{o as default};
--- assets/ProjectDetails-24eb37df.js
+++
@@ -1,1 +0,0 @@
-import{b as t,j as o}from"./index-373bbef3.js";const e=()=>{const{projectId:s}=t();return o.jsxs("div",{children:["Now showing project with id - ",s]})};export{e as default};
