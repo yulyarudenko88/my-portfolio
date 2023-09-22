9fc9f412eacfe9f15d6fd2d871466c20703e1301
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-09-22 16:59:35 +0300

    add: loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,13 +3,32 @@ import { Suspense } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { Wrapper, Main } from './SharedLayout.styled';
+import { Watch } from 'react-loader-spinner';
 
 const SharedLayout = () => {
   return (
     <Wrapper>
       <Header />
       <Main>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <div>
+              <Watch
+                height="70"
+                width="70"
+                radius="45"
+                color="rgb(209, 209, 209)"
+                ariaLabel="loading"
+                wrapperStyle={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  paddingTop: '75px',
+                }}
+                visible={true}
+              />
+            </div>
+          }
+        >
           <Outlet />
         </Suspense>
       </Main>
