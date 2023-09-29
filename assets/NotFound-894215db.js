063e9e0352f0d4366c169841928d40de838a07e5
yulyarudenko88 <yulyarudenko88@users.noreply.github.com>
2023-09-29 14:34:18 +0000

    deploy: c5c8881e2ddefa3468770231eac50581b230a723

---
+++ assets/NotFound-894215db.js
@@ -0,0 +1,13 @@
+import{n as e,j as t,C as n}from"./index-83a82186.js";const s=e.p`
+  font-size: 14px;
+  line-height: 2;
+  text-align: center;
+
+  span {
+    font-weight: 600;
+  }
+`,o=e.img`
+width: 200px;
+height: 200px;
+margin: 0 auto 12px auto;
+`,a="/my-portfolio/assets/cat-e0e618d6.svg",i=()=>t.jsxs(n,{children:[t.jsx(o,{src:a,alt:"cat"}),t.jsxs(s,{children:[t.jsx("span",{children:"We are sorry,"})," but the page you were looking for can’t be found.."]})]});export{i as default};
--- assets/ProjectLinks-ae1ed9c2.js
+++ assets/ProjectLinks-ae1ed9c2.js
@@ -1,4 +1,4 @@
-import{G as d,n as l,P as t,j as n,I as g,c as p}from"./index-ed2d9af2.js";function u(r){return d({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"}}]})(r)}const b=`
+import{G as d,n as l,P as t,j as n,I as g,c as p}from"./index-83a82186.js";function u(r){return d({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"}}]})(r)}const b=`
   display: flex;
   gap: 16px;
 `,h=`
